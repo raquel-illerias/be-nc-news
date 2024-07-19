a2a92a3457c6ba524aac3adf192e14fbaddd3968
Raquel Illerias <raquel.illerias@gmail.com>
2024-07-19 12:21:46 +0100

    files setup for hosting

--- listen.js
+++ listen.js
@@ -1,6 +1,4 @@
-const app = require("./app");
+const app = require('./app.js');
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Listen on ${PORT}.`);
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
